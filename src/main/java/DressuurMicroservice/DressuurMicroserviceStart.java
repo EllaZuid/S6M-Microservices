fac0151203cc3a786261ada600fa4844076cffaf
Ella Zuidema <ella.zuidema@student.fontys.nl>
2021-10-08 12:56:35 +0200

    initial setup dressuur microservice

---
+++ src/main/java/DressuurMicroservice/DressuurMicroserviceStart.java
@@ -0,0 +1,13 @@
+package DressuurMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DressuurMicroserviceStart {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DressuurMicroserviceStart.class, args);
+	}
+
+}
---
+++ src/test/java/DressuurMicroservice/DressuurMicroserviceStartTests.java
@@ -0,0 +1,13 @@
+package DressuurMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DressuurMicroserviceStartTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
